2621bff556ee145de267177eb52751b548c2ef0e
engin-eer <engin.aydgdu@gmail.com>
2021-03-18 22:55:28 +0300

    13 Mart 2021 son

--- src/app/components/cart-summary/cart-summary.component.ts
+++ src/app/components/cart-summary/cart-summary.component.ts
@@ -25,6 +25,7 @@ export class CartSummaryComponent implements OnInit {
 
   removeFromCart(product:Product){
     this.cartService.removeFromCart(product);
-    this.toastrService.error(product.productName,"Silindi")
+    this.toastrService.error("Silindi", product.productName)
   }
 }
+ 
